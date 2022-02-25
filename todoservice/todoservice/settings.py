81d38af6593adff69f5dead2ea951573b61a4f73
semenov-lev <firemail.leo@gmail.com>
2022-02-25 16:24:49 +0300

    added Simple JWT

--- todoservice/todoservice/settings.py
+++ todoservice/todoservice/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'usersapp',
     'projectsapp',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -144,6 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
