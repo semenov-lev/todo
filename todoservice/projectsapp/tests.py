7f1c6660ae833b003e54fe1de961393cf685e9f1
semenov-lev <firemail.leo@gmail.com>
2022-03-09 16:29:17 +0300

    added APIRequestFactory adn APIClient tests

--- todoservice/projectsapp/tests.py
+++ todoservice/projectsapp/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from datetime import datetime
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from usersapp.models import User
+from .views import ProjectModelViewSet
+from .models import Project
 
-# Create your tests here.
+
+class TestProjectsViewSet(TestCase):
+
+    def setUp(self):
+        self.url = '/api/projects/'
+        self.user = User.objects.create_user('test-user', 'test-user@test.com', 'test')
+        self.project = Project.objects.create(name='test-project', deadline_timestamp=datetime(2025, 5, 12))
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        client = APIClient()
+        client.login(username='test-user', password='test')
+        response = client.get(f'{self.url}{self.project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        client.logout()
--- todoservice/usersapp/tests.py
+++ todoservice/usersapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from .views import UserViewSet
+from .models import User
 
-# Create your tests here.
+
+class TestUserViewSet(TestCase):
+
+    def setUp(self):
+        self.url = '/api/users/'
+        self.admin = User.objects.create_user('test-admin', 'test-admin@test.com', 'test')
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        force_authenticate(request, user=self.admin)
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
