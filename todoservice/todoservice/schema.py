6d02c9398e0f905bd551c2054fed332f0e0a5808
semenov-lev <firemail.leo@gmail.com>
2022-03-22 16:08:51 +0300

    added graphQL list schema with user, project and todo models

--- todoservice/todoservice/schema.py
+++ todoservice/todoservice/schema.py
@@ -1,9 +1,41 @@
 import graphene
-from graphene import Schema, String
+from graphene_django import DjangoObjectType
+
+from projectsapp.models import Project, ToDo
+from usersapp.models import User
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ToDoType(DjangoObjectType):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    hello = String(default_value='Hi!')
+    all_users = graphene.List(UserType)
+    all_projects = graphene.List(ProjectType)
+    all_todos = graphene.List(ToDoType)
+
+    def resolve_all_users(root, info):
+        return User.objects.all()
+
+    def resolve_all_projects(root, info):
+        return Project.objects.all()
+
+    def resolve_all_todos(root, info):
+        return ToDo.objects.all()
 
 
-schema = Schema(query=Query)
+schema = graphene.Schema(query=Query)
