4fbf652fa2f2cb02e64ba87a03e85ccf4d32b77c
semenov-lev <firemail.leo@gmail.com>
2022-03-28 12:50:19 +0300

    added graphql mutations

--- todoservice/projectsapp/models.py
+++ todoservice/projectsapp/models.py
@@ -21,7 +21,7 @@ class Project(models.Model):
     description = models.TextField(verbose_name='Описание', blank=True)
     rep_url = models.URLField(verbose_name='Ссылка на репозиторий', blank=True)
     status = models.CharField(verbose_name='Статус', choices=STATUS_CHOICES, max_length=3, default='PRC')
-    deadline_timestamp = models.DateTimeField(verbose_name='Дата сдачи', blank=True)
+    deadline_timestamp = models.DateTimeField(verbose_name='Дата сдачи', blank=True, null=True)
     create_timestamp = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     update_timestamp = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
 
